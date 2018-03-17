7d350a3088070713a845f15a7cee6792c856319d
Cristian Botella <cri.bh6@gmail.com>
2018-03-17 10:48:44 +0100

    ApiService

--- src/app/shared/services/api/api.service.ts
+++ src/app/shared/services/api/api.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs/Observable';
+import {
+  Http,
+  Headers,
+  ConnectionBackend,
+  RequestOptions
+} from '@angular/http';
+import 'rxjs/add/operator/map';
 
-//endpoints que se saltan la autentificacion pass through
+// endpoints que se saltan la autentificacion pass through
 const passthrough = ['login', 'signup'];
 
 @Injectable()
-export class ApiService extends HttpClient {
+export class ApiService extends Http {
+  token = '';
+  // provide como factory
 
-    token: string = '';
-    // provide como factory
-
-  constructor() {
-    super();
+  constructor(backend: ConnectionBackend, options: RequestOptions) {
+    super(backend, options);
   }
 
   static buildUrl(path) {
@@ -21,48 +27,89 @@ export class ApiService extends HttpClient {
       : `${environment.apiPath}/${path}`;
   }
   static isBasePath(path) {
-    return path.startsWith('https://') || path.startsWith('http://');
+    return path.startsWith('http://') || path.startsWith('https://');
   }
-  private toQuery(params, key, search) {
-    //coger del codesandbox
-  }
-  private toBody(params){}
-  private toJson(){}
-  
-  get(url, params = {}) {
-    const apiUrl = ApiService.buildUrl(url);
-    const query = this.toQuery(params);
-    if(!passthrough.indexOf(url) === -1){
-        //firmamos
-        
+  private toBody = (params: Object): string =>
+    params ? JSON.stringify(params) : '';
+  private toQuery(
+    params: Object | any[],
+    key: string = null,
+    search = new URLSearchParams('')
+  ): URLSearchParams {
+    if (params === undefined) {
+      return search;
     }
-    return super.get(apiUrl, {params: query})
-        .map(this.toJson);
+    if (params instanceof Array && key) {
+      // If it is an array, add brackets with the indexes: param[0]: value
+      params.forEach(
+        (p, i) => (search = this.toQuery(p, `${key}[${i}]`, search))
+      );
+    } else if (params instanceof Object) {
+      // If it is an object, add brackets wit the keys: param[key]: value
+      Object.keys(params).forEach(
+        objKey =>
+          (search = this.toQuery(
+            params[objKey],
+            key ? `${key}[${objKey}]` : objKey,
+            search
+          ))
+      );
+    } else if (key) {
+      // If the value is not an object nor and array, just append it with its key
+      if (params === true) {
+        // Transforming trues to 1
+        search.append(key, '1');
+      } else if (params === false) {
+        // And falses to 0 (if necessary)
+        search.append(key, '0');
+      } else {
+        search.append(key, params);
+      }
+    }
+    return search;
+  }
+
+  get(url, params = {}) {
+    const options: any = {};
+    const apiUrl = ApiService.buildUrl(url);
+    options.params = this.toQuery(params);
+    if (passthrough.indexOf(url) === -1) {
+      if (!this.token) {
+        return Observable.throw({ message: 'Must login' });
+      }
+      options.headers = new Headers();
+      options.headers.set('Authorization', `Bearer ${this.token || ''}`);
+    }
+    return super.get(apiUrl, options).map(res => res.json());
   }
   post(url, params = {}) {
     const apiUrl = ApiService.buildUrl(url);
     const body = this.toBody(params);
-    if(!passthrough.indexOf(url) === -1){
-        //firmamos
+    if (passthrough.indexOf(url) === -1) {
+      // firmamos
     }
-    return super.post(apiUrl, body)
-        .map(this.toJson);
+    return super.post(apiUrl, body).map(res => res.json());
   }
-  put(url, params) {
+  put(url, params = {}) {
     const apiUrl = ApiService.buildUrl(url);
     const body = this.toBody(params);
-    return super.put(apiUrl, body)
-        .map(this.toJson);
+    if (passthrough.indexOf(url) === -1) {
+      // firmamos
+    }
+    return super.put(apiUrl, body).map(res => res.json());
   }
-  delete(url, params) {
+  delete(url) {
     const apiUrl = ApiService.buildUrl(url);
-    return super.delete(apiUrl)
-        .map(this.toJson);
+    if (passthrough.indexOf(url) === -1) {
+      // firmamos
+    }
+    return super.delete(apiUrl).map(res => res.json());
   }
 
   login(user: string, password: string) {
-      this.post('login', {user, password}).do(
-          res => this.token = res.data.token;
-      );
+    this.post('login', { user, password }).do(res => (this.token = res.token));
+  }
+  logout() {
+    this.post('logout').do(() => (this.token = ''));
   }
 }
