3884afc28a412cf7738a68332360115a079fc688
Cristian Botella <cri.bh6@gmail.com>
2018-06-12 23:53:38 +0200

    Authors list component generation

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-authors',
+  selector: 'uaz-authors',
   templateUrl: './authors.component.html',
   styleUrls: ['./authors.component.css']
 })
--- src/app/authors/authors.service.ts
+++ src/app/authors/authors.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
 import { Author } from '../shared/models/author.model';
 import { ApiService } from '../shared/services/api/api.service';
 import { ResourceService } from '../shared/services/api/resource.service';
