7d05d541a908d817b0a0527f530b43fe8521cbdc
Cristian Botella <cri.bh6@gmail.com>
2018-06-14 23:57:14 +0200

    Add authors create component logic

--- src/app/authors/authors-create/authors-create.component.ts
+++ src/app/authors/authors-create/authors-create.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthorsService } from '../authors.service';
+import { AuthService } from '../../shared/services/api/auth.service';
+import { Author } from '../../shared/models/author.model';
 @Component({
-  selector: 'app-authors-create',
+  selector: 'uaz-authors-create',
   templateUrl: './authors-create.component.html',
   styleUrls: ['./authors-create.component.css']
 })
 export class AuthorsCreateComponent implements OnInit {
+  public author: Author;
 
-  constructor() { }
+  constructor(    private _toastr: ToastrService,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _authService: AuthService,
+    private _athorsService: AuthorsService) { }
 
   ngOnInit() {
+    this.author = new Author({});
+    this.author.init();
   }
 
+  onSubmit(form) {
+    this._athorsService.create(this._authService.getToken(), this.author).subscribe(
+      response => {
+        this._toastr.success('Autor creado correctamente');
+        this._router.navigate(['authors']);
+      },
+      error => {
+        this._toastr.error('Se ha producido un error inesperado al crear el autor');
+        console.log(error as any);
+      }
+    );
+  }
 }
--- src/app/shared/models/author.model.ts
+++ src/app/shared/models/author.model.ts
@@ -1,11 +1,14 @@
 import { Resource } from './resource';
 
 export class Author extends Resource {
-  id: number;
   nombre: string;
 
   constructor(obj: object) {
     super();
     Object.assign(this, obj);
+  }
+
+  init() {
+    this.nombre = null;
   }
 }
