2bff0e595ddd8745e3e36cad941ca4227b5536fa
Cristian Botella <cri.bh6@gmail.com>
2018-06-14 23:44:19 +0200

    Add authors-create generated component

---
+++ src/app/authors/authors-create/authors-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors-create',
+  templateUrl: './authors-create.component.html',
+  styleUrls: ['./authors-create.component.css']
+})
+export class AuthorsCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/authors/authors.module.ts
+++ src/app/authors/authors.module.ts
@@ -3,13 +3,14 @@ import { SharedModule } from '../shared/shared.module';
 import { AuthorsService } from './authors.service';
 import { AuthorsComponent } from './authors.component';
 import { AuthorsListComponent } from './authors-list/authors-list.component';
+import { AuthorsCreateComponent } from './authors-create/authors-create.component';
 // import { authorsCreateComponent } from './authors-create/authors-create.component';
 // import { authorsDetailComponent } from './authors-detail/authors-detail.component';
 
 @NgModule({
   imports: [SharedModule],
   providers: [AuthorsService],
-  declarations: [AuthorsComponent, AuthorsListComponent],
+  declarations: [AuthorsComponent, AuthorsListComponent, AuthorsCreateComponent],
   exports: [AuthorsComponent]
 })
 export class AuthorsModule {}
